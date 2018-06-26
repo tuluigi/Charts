67c7069ce2943bc19a5bab1c3d4993d65e262ac4
luigi <>
2018-06-26 09:30:01 +0800

    add commit file

---
+++ ChartsDemo-iOS/Objective-C/TestMyLineChartView.swift
@@ -0,0 +1,21 @@
+//
+//  TestMyLineChartView.swift
+//  ChartsDemo-iOS
+//
+//  Created by luigi on 2018/6/26.
+//  Copyright © 2018年 dcg. All rights reserved.
+//
+
+import UIKit
+import Charts
+class TestMyLineChartView: LineChartView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
